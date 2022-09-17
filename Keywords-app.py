542c6d4abd2578bb2733bda5acc3c23659ed1573
Ali Reza Moharam Ali <74486541+alirezaimmune@users.noreply.github.com>
2022-09-17 15:16:46 +0400

    Update Keywords-app.py

--- Keywords-app.py
+++ Keywords-app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-nltk.download('stopwords')
+
 st.set_page_config(page_title='The Keyword Extraction App',
     layout='wide')
 
