e6dd6ff1d90c64bf771def20edafb6d16e0fd44e
Ali Reza Moharam Ali <74486541+alirezaimmune@users.noreply.github.com>
2022-10-06 20:42:47 +0400

    Update Keywords-app.py

--- Keywords-app.py
+++ Keywords-app.py
@@ -1,13 +1,11 @@
-import streamlit as st
+iimport streamlit as st
 import pandas as pd
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-
 st.set_page_config(page_title='The Keyword Extraction App',
     layout='wide')
-
 
 st.write("""
 # 
@@ -20,15 +18,20 @@ with st.sidebar.header('1. Upload your CSV data'):
 
 
 st.subheader('1. Dataset')
+global df
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.markdown('**1.1. Glimpse of dataset**')
+    print(uploaded_file)
+
+    try:
+        df=pd.read_csv(uploaded_file)
+    except Exception as e:
+        print(e)
+        df=pd.read_csv(uploaded_file)
+try:
     st.write(df)
-    
-  
-else:
-    st.info('Awaiting for CSV file to be uploaded.')
-    
+except Exception as e:
+    print(e)
+    st.write("Waiting to upload file")
 
 st.subheader('2. Keywords')
 
@@ -41,10 +44,22 @@ def get_keywords(row):
     keywords = [keyword for keyword in tokens if keyword.isalpha() and not keyword in stop_words]
     keywords_string = ','.join(keywords)
     return keywords_string
+if uploaded_file is not None:
+    print(uploaded_file)
+    try:
+        df['Keywords'] = df.apply(get_keywords, axis=1)
+        df
+    except Exception as e:
+        print(e)
+        df=pd.read_csv(uploaded_file)
 
-df['Keywords'] = df.apply(get_keywords, axis=1)
-
-df
 
 
-st.download_button(label="Download the Extracted Keywords",data=df.to_csv(),mime='text/csv')
+
+if uploaded_file is not None:
+    print(uploaded_file)
+    try:
+        st.download_button(label="3.Download the Extracted Keywords",data=df.to_csv(),mime='text/csv')
+    except Exception as e:
+        print(e)
+        df=pd.read_csv(uploaded_file)
